8905d221047b0cdd195f0c87397f08ccfa63d22e
Alexandros Karatzaferis <ekaratzaferis@gmail.com>
2026-04-13 01:03:09 +0300

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://duditeria.com',
   output: 'static',
-  outDir: './docs',
   build: {
     assets: 'assets',
   },
